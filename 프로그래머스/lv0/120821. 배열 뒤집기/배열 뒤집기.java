507fe2fedecdbf44e8617c5bc897b6822ac6fe38
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-01-06 23:20:15 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        int l = num_list.length;
+        answer = new int[l];
+        for (int i=0; i<l; i++) {
+            answer[i] = num_list[l-i-1];
+        }
+        return answer;
+    }
+}
