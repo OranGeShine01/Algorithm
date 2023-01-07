cf99deeef8bc86042b78d2a5c11ad1c19c8cd7b8
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-01-08 03:45:51 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+        String str = String.valueOf(num);
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i)==k+48) {
+                answer=i+1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
