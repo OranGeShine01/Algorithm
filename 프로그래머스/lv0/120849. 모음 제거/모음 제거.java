3571723738fcc449470d6d9f2d77f5183b0c6975
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-01-07 13:31:33 +0900

    [level 0] Title: 모음 제거, Time: 0.06 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        my_string = my_string.replace("u", "");
+        my_string = my_string.replace("a", "");
+        my_string = my_string.replace("e", "");
+        my_string = my_string.replace("i", "");
+        my_string = my_string.replace("o", "");
+        
+        answer = my_string;
+        
+        return answer;
+    }
+}
