8f37a77b8ddde05b7dc40e73739d797fcd749064
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-01-16 23:13:03 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 280 ms, Memory: 29188 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -25,15 +25,16 @@ public class Main {
     }
 
     static int w(int a, int b, int c) {
-        if (range(a,b,c) && w[a][b][c] != 0) {
-            return w[a][b][c];
-        }
-        else if (a<=0 || b<=0 || c<=0) {
+        
+        if (a<=0 || b<=0 || c<=0) {
             return 1;
         }
         else if (a>20 || b>20 || c>20) {
             w[20][20][20] = w(20, 20, 20);
             return w[20][20][20];
+        }
+        else if (w[a][b][c] != 0) {
+            return w[a][b][c];
         }
         else if (a < b && b < c) {
             w[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
@@ -43,9 +44,5 @@ public class Main {
             w[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
             return w[a][b][c];
         }
-    }
-
-    static boolean range(int a, int b, int c) {
-        return 0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20;
     }
 }
