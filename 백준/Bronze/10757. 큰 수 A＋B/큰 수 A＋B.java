9d25cc3de1a34e114204546c18469c0db5e33bdd
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-01-22 19:45:43 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 256 ms, Memory: 17288 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str = (br.readLine()).split(" ");
+		BigInteger bint = new BigInteger(str[0]);
+		BigInteger bint2 = new BigInteger(str[1]);
+		
+		System.out.println(bint.add(bint2));		
+
+	}
+
+}
