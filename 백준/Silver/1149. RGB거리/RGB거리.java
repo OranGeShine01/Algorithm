4071f4da75ebad95548fae0969ff2514908dd0d0
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-08-24 23:22:11 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14536 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -5,50 +5,31 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] cost; // 각 비용을 담을 배열
-    static int n;
+    // 각 집의 색별 비용, 빨강, 초록, 파랑 순.
+    static int[][] cost;
+    // n번째 집까지, 규칙에 따라 칠하는 최소비용을 메모이제이션할 dp (각 열은 마지막 집의 색)
     static int[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
         cost = new int[n][3];
-        dp = new int[n][3];
-
+        dp = new int[n+1][3];
         for (int i=0; i<n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j=0; j<=2; j++) {
-                cost[i][j] = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j=0; j<3; j++) {
+                 cost[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        for (int i=0; i<=2; i++) {
-            dp[0][i] = cost[0][i];
+        for (int i=1; i<n+1; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i-1][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i-1][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i-1][2];
         }
 
-        System.out.println(Math.min(Math.min(coloring(n-1, 0), coloring(n-1, 1)), coloring(n-1, 2)));
+        System.out.println(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
+
     }
 
-    // 색칠하기
-    static int coloring(int n, int color) {
 
-        // 탐색하지 않은 배열인 경우, 탐색하여 값을 입력한다.
-        if (dp[n][color] == 0) {
-
-            if (color==0) {
-                dp[n][color] = Math.min(coloring(n-1, 1), coloring(n-1, 2)) + cost[n][color];
-            }
-
-            else if (color==1) {
-                dp[n][color] = Math.min(coloring(n-1, 0), coloring(n-1, 2)) + cost[n][color];
-            }
-
-            else if (color==2) {
-                dp[n][color] = Math.min(coloring(n-1, 0), coloring(n-1, 1)) + cost[n][color];
-            }
-        }
-        
-        return dp[n][color];
-    }
 }
-
