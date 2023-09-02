f39502f2f0d4198428a7caace96b4b977facc338
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-09-02 18:11:49 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 956 ms, Memory: 132992 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,22 +2,17 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int n, m;
-    static int[][] arr;
-    static int[][] ps;
-
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n][n];
-        ps = new int[n+1][n+1];
+        int[][] arr = new int[n][n];
+        int[][] ps = new int[n+1][n+1];
         for (int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
             int count = 0;
