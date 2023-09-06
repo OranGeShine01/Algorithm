4abe4ccfab85bcf82a601e57461fb697f119ba22
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-09-06 21:03:48 +0900

    [Bronze II] Title: 공 바꾸기, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = i+1;
+        }
+
+        for (int k=0; k<m; k++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int temp = arr[i-1];
+            arr[i-1] = arr[j-1];
+            arr[j-1] = temp;
+        }
+
+        for (int i : arr) {
+            bw.append(String.valueOf(i)).append(" ");
+        }
+
+        bw.close();
+        br.close();
+    }
+}
