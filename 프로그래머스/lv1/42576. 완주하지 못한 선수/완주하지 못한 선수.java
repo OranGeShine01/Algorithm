b5a4e85df1849e8a9e4337fdcb9d2c790e96ffc5
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-09-07 22:38:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.16 ms, Memory: 94.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        for (String str : participant) {
+            map.put(str, map.getOrDefault(str, 0)+1);
+        }
+        for (String str : completion) {
+            map.put(str, map.get(str)-1);
+        }
+        for (String str : participant) {
+            if (map.get(str)==1) {
+                answer = str;
+                break;
+            }
+        }
+        return answer;
+    }
+}
