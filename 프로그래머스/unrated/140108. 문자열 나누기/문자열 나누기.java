90f921ef22342b3894b9c0bcfd6a001956ea510b
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-09-09 02:08:24 +0900

    [unrated] Title: 문자열 나누기, Time: 0.75 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,29 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int fc = 0; // first count
+        int dc = 0; // diff count
+        char first=' ';
+        int l = s.length();
+        for (int i=0; i<l; i++) {
+            char ch = s.charAt(i);
+            if (fc==0) {
+                first=ch;
+                fc++;
+            } else if (fc==dc) {
+                answer++;
+                fc=0;
+                dc=0;
+                i--;
+            } else if (ch==first) {
+                fc++;
+            } else {
+                dc++;
+            }
+        }
+        if (fc+dc>0) answer++;
+        return answer;
+    }
+}
