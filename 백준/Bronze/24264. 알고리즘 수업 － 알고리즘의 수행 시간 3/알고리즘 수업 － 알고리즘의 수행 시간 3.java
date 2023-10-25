2a33d35f7e8dfafd1c07566178c87f05de7ab269
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-10-25 20:36:41 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,17 @@
+    import java.io.*;
+
+    public class Main {
+
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            long n = Long.parseLong(br.readLine());
+            StringBuilder sb = new StringBuilder();
+            sb.append(n*n).append('\n').append('2');
+            bw.append(sb);
+            bw.close();
+            br.close();
+        }
+    }
