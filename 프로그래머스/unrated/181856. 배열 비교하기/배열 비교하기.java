5e7894a73f68f97e3e48d67b97b8433bbbdcc19e
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2023-11-23 20:46:56 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length>arr2.length) return 1;
+        else if (arr1.length<arr2.length) return -1;
+        
+        int[] sum = new int[2];
+        
+        for (int i=0; i<arr1.length; i++) {
+            sum[0]+=arr1[i];
+            sum[1]+=arr2[i];
+        }
+        
+        if (sum[0]>sum[1]) return 1;
+        else if (sum[0]<sum[1]) return -1;
+        else return 0;
+    }
+}
