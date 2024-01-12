c23877df5f2bdb10153446bf66465f4bddf7a0e2
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-01-12 20:24:15 +0900

    [level Lv.2] Title: [3차] n진수 게임, Time: 2.78 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/Lv.2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/Lv.2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,38 +1,18 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder sb = new StringBuilder();
-        String str = game(n, m*t+p);
-        System.out.println(str);
-        p--;
-        int l = str.length();
-        for (int i=0; i<t; i++) {
-            sb.append(str.charAt(i*m+p));
-        }
-        return sb.toString();
-    }
-    
-    public String game(int n, int l) {
-        StringBuilder sb = new StringBuilder();
         int i = 0;
+        int l = m*t;
         while (sb.length()<l) {
-            sb.append(notation(n, i));
-            i++;
+            sb.append(Integer.toString(i++, n));
         }
-        return sb.toString();
+        System.out.println(sb);
+        p--;
+        StringBuilder sb2 = new StringBuilder();
+        for (int j=0; j<l; j+=m) {
+            sb2.append(sb.charAt(j+p));
+        }
+        return sb2.toString().toUpperCase();
     }
     
-    public String notation (int n, int t) {
-        StringBuilder sb = new StringBuilder();
-        if (t==0) return String.valueOf(t);
-        while (t!=0) {
-            sb.append(one(t%n));
-            t/=n;
-        }
-        return sb.reverse().toString();
-    }
-    
-    public String one (int t) {
-        if (t<10) return String.valueOf(t);
-        else return String.valueOf((char)(t-10+'A'));
-    }
 }
