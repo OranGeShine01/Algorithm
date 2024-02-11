54a9f328e2e2633ca03c5f7c6d79477afaf7f302
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-02-11 19:30:41 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        // a : 빈병의 수 b : 바꿔주는 새로운 콜라의 수 n : 초기 콜라의 수
-        int answer = 0;
-        while(n>=a) {
-            int k = n/a*b;
-            n = k + n%a;
-            answer+=k;
-        }
-        return answer;
+        return (n > b ? n - b : 0) / (a - b) * b;
     }
 }
