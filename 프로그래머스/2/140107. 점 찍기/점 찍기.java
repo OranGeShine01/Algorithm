a12d04f5db52410175c57eda503c802fdc57236e
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-02-17 19:56:52 +0900

    [level 2] Title: 점 찍기, Time: 0.08 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,8 +1,9 @@
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
+        long dd = (long)d*d;
         for (long i=0; i<=d; i+=k) {
-            long x = (long)Math.sqrt((long)d*d-i*i)/k;
+            long x = (long)Math.sqrt(dd-i*i)/k;
             answer+=x+1;
         }
         return answer;
