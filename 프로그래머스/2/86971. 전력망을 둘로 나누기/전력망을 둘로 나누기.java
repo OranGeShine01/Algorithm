d194a056503fe35e683d26c69fd64d376215e888
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-02-25 13:10:48 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.37 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,61 +1,29 @@
-import java.util.*;
-
 class Solution {
-    
-    int[][] arr;
-    
-    public int solution(int n, int[][] wires) {
-        int answer = n;
-        arr = new int[n+1][n+1];
-        boolean[] check = new boolean[n+1];
-        
-        for (int i=0; i<wires.length; i++) {
-            arr[wires[i][0]][wires[i][1]] = 1;
-            arr[wires[i][1]][wires[i][0]] = 1;
-        }
-        
-        for (int i=0; i<wires.length; i++) {
-            
-            int left = wires[i][0];
-            
-            //if (check[left]) continue;
-            
-            int right = wires[i][1];
-            
-            arr[left][right] = 0;
-            arr[right][left] = 0;
-            
-            answer = Math.min(answer, bfs(left, n));                
-            check[left] = true;
-            
-            arr[left][right] = 1;
-            arr[right][left] = 1;
-        }
-        
-        return answer;
-    }
-    
-    int bfs(int left, int n) {
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[n+1];
-        int count = 1;
-        
-        q.add(left);
-        visited[left] = true;
-        
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            for (int i=1; i<n+1; i++) {
-                if (arr[node][i]==1 && !visited[i]) {
-                    q.add(i);
-                    visited[i] = true;
-                    count++;
-                }
+    int N, answer;
+    int[][] map;
+    boolean[] vst;
+    int dfs(int n){
+        vst[n] = true;
+        int child = 1;
+        for(int i = 1; i <= N; i++) {
+            if(!vst[i] && map[n][i] == 1) {
+                vst[i] = true;
+                child += dfs(i);
             }
         }
-        
-        return Math.abs(count*2-n);
+        answer = Math.min(answer, Math.abs(2 * child - N));
+        return child;
     }
-    
-    
+    public int solution(int n, int[][] wires) {
+        N = n;
+        answer = n;
+        map = new int[n+1][n+1];
+        vst = new boolean[n+1];
+        for(int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+            map[a][b] = map[b][a] = 1;
+        }
+        dfs(1);
+        return answer;
+    }
 }
