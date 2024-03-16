170b26b6669fde845c637608f9aa6a632debbea2
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-03-16 18:43:18 +0900

    [level 2] Title: 문자열 압축, Time: 0.06 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,45 +1,33 @@
-import java.util.*;
-
 class Solution {
-    
     public int solution(String s) {
-        final int limit = 1001;
-        int answer = limit;
-        int len = s.length();
-        if (len==1) return 1;
-        for (int i=1; i<=len/2; i++) {
-            answer = Math.min(answer, extract(s, i));
-        }
-        return answer;
-    }
-    
-    int extract (String s, int n) {
-        int len = s.length();
-        StringBuilder sb = new StringBuilder();
-        String unit = s.substring(0, n);
-        Queue<String> q = new LinkedList<>();
-        int cnt = 0;
-        
-        for (int i=0; i<len; i+=n) {
-            q.add(s.substring(i, Math.min(i+n, len)));
-        }
-        
-        while (!q.isEmpty()) {
-            if (unit.equals(q.peek())) {
-                q.poll();
-                cnt++;
-            } else {
-                if (cnt>1) sb.append(cnt);
-                sb.append(unit);
-                unit = q.peek();
-                cnt = 0;
+        int min = s.length();
+        int len = s.length()/2+1;
+        for(int i = 1; i < len; i++) {
+            String before = "";
+            int sum = 0;
+            int cnt = 1;
+            for(int j = 0; j < s.length();) {               
+                int start = j;
+                j = (j+i > s.length()) ? s.length():j+i;
+                String temp = s.substring(start, j);
+                if(temp.equals(before)) {
+                    cnt++;
+                } else {
+                    if(cnt != 1) {
+                        sum += (int)Math.log10(cnt)+1;
+                    }
+                    cnt = 1;
+                    sum+=before.length();
+                    before = temp;
+                }
             }
+            sum+=before.length();
+            if(cnt != 1) {
+                sum += (int)Math.log10(cnt)+1;
+            }
+            min = (min > sum) ? sum : min;
         }
-        
-        if (cnt>1) sb.append(cnt);
-        sb.append(unit);
-        
-        return sb.length();
+
+        return min;
     }
-    
 }
