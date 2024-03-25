9ee451dce11513b01915d7b7ebe65f911a236ab6
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-03-25 21:23:30 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        int len = number.length();
+        for (int i=0; i<len; i++) {
+            answer += number.charAt(i) - '0';
+        }
+        return answer%9;
+    }
+}
