ad04ee9788c8a98d8219520e1b222a572514ab20
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-04-30 20:42:14 +0900

    [level 3] Title: 숫자 게임, Time: 59.17 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int idxA = 0;
+        int idxB = 0;
+        
+        while (idxB < B.length) {
+            if (B[idxB] > A[idxA]) {
+                answer++;
+                idxA++;
+            }
+            idxB++;
+        }
+        
+        return answer;
+    }
+}
