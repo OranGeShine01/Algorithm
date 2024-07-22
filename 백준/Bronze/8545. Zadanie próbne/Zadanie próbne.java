5b6b7ea24f95846c3c7cd42510f1690012618c1f
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-07-22 20:44:37 +0900

    [Bronze V] Title: Zadanie próbne, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.append(new StringBuilder(br.readLine()).reverse());
+
+        bw.close();
+
+    }
+}
