d427c1eae07b328ef38cd1992c733ce6c10c54a7
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-07-26 21:27:34 +0900

    [Bronze V] Title: 제리와 톰, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] str = br.readLine().split(" ");
+        int[] arr = new int[str.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        bw.append(String.valueOf(arr[1] - arr[0])).append(" ").append(str[1]);
+        bw.close();
+
+    }
+}
