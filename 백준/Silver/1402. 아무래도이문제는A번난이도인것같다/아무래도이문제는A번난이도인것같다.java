69b0d336e31e4195c14067256e7103fbc9d5360c
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-08-27 21:26:36 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            sb.append("yes\n");
+        }
+
+        bw.append(sb);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
