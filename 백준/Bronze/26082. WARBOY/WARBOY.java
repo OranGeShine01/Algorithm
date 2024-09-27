54a91b06bc2782fcde0648ac83154f190a199d29
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-09-27 19:32:46 +0900

    [Bronze V] Title: WARBOY, Time: 100 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] str = br.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        int c = Integer.parseInt(str[2]);
+
+        bw.append(String.valueOf(b / a * 3 * c));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
