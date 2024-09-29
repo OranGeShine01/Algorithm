e9faa407fceb83002a4a950af16f8af939c249dd
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-09-29 21:12:10 +0900

    [Silver III] Title: Four Squares, Time: 136 ms, Memory: 15428 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                int index = i - j * j;
+                min = Math.min(min, dp[index]);
+            }
+            dp[i] = min + 1;
+        }
+
+        bw.append(String.valueOf(dp[n]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
