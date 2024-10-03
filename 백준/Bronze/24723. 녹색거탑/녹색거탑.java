cfa450d7478dce0f3695b38afd9a1b62b3c16e25
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-10-03 09:37:57 +0900

    [Bronze IV] Title: 녹색거탑, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.append(String.valueOf((int)Math.pow(2, n)));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
