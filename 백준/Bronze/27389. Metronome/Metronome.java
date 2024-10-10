7d29e76559b38e74e409719629b5a54d78bb6551
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-10-10 20:08:48 +0900

    [Bronze V] Title: Metronome, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.append(String.valueOf(n/4.0));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
