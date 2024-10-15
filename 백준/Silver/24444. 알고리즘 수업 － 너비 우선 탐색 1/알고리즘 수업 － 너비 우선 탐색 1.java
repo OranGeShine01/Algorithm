de177a38b9291f1ac838db22da1eb5a20208630d
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-10-15 21:12:41 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 972 ms, Memory: 106364 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,72 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] nmr = br.readLine().split(" ");
+        int n = Integer.parseInt(nmr[0]);
+        int m = Integer.parseInt(nmr[1]);
+        int r = Integer.parseInt(nmr[2]);
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            String[] str = br.readLine().split(" ");
+            int u = Integer.parseInt(str[0]);
+            int v = Integer.parseInt(str[1]);
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        int[] answer = bfs(graph, r);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            sb.append(answer[i]).append('\n');
+        }
+
+        bw.append(sb);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static int[] bfs(List<List<Integer>> graph, int start) {
+        int n = graph.size() - 1;
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> q = new LinkedList<>();
+        int[] result = new int[n + 1];
+        visited[start] = true;
+        q.add(start);
+        int count = 1;
+        result[start] = count;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int i : graph.get(node)) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    q.add(i);
+                    count++;
+                    result[i] = count;
+                }
+            }
+        }
+        return result;
+    }
+}
