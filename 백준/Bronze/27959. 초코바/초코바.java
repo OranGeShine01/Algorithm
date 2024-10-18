b298d191fd98753f53d68a9ef84921e323cfe379
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-10-18 21:14:07 +0900

    [Bronze V] Title: 초코바, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] nm = br.readLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+
+        String answer = n * 100 >= m ? "Yes" : "No";
+
+        bw.append(answer);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
