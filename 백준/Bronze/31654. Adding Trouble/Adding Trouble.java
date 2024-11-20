681a786892d4bd03cfdcce84a8351067b716b10c
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2024-11-20 23:22:22 +0900

    [Bronze V] Title: Adding Trouble, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] abc = br.readLine().split(" ");
+        int a = Integer.parseInt(abc[0]);
+        int b = Integer.parseInt(abc[1]);
+        int c = Integer.parseInt(abc[2]);
+
+        bw.append(a + b == c ? "correct!" : "wrong!");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
