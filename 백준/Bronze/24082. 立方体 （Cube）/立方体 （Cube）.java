b6827daa8e773aed5b9a4eb8529cd8afcce5a548
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2025-08-21 23:47:03 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 100 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,16 @@
+    import java.io.*;
+
+    public class Main {
+
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int input = Integer.parseInt(br.readLine());
+
+            bw.append(String.valueOf(input * input * input));
+            bw.close();
+            br.close();
+        }
+    }
