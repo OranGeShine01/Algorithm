5876c5452cce340ea622a68493f0d5c0bd431ff5
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2025-11-10 23:56:55 +0900

    [Bronze V] Title: Java Warriors, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(br.readLine());
+
+        bw.append(String.valueOf(input * 4000));
+        bw.close();
+        br.close();
+    }
+}
