90657a36c50d6b66162c8476b14be4d215704836
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2026-09-10 19:09:08 +0900

    [level 2] Title: 가장 큰 수, Time: 129.70 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,22 +2,17 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        int l = numbers.length;
-        String[] str = new String[l];
-        for (int i=0; i<l; i++) {
-            str[i] = String.valueOf(numbers[i]);
+        StringBuilder answer = new StringBuilder();
+        int len = numbers.length;
+        String[] arr = new String[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = String.valueOf(numbers[i]);
         }
-        Arrays.sort(str, new Comparator<String>() {
-           @Override
-            public int compare(String str1, String str2) {
-                return (str2+str1).compareTo(str1+str2);
-            }
-        });
-        if (str[0].equals("0")) return "0";
-        StringBuilder sb = new StringBuilder();
-        for (String i : str) {
-            sb.append(i);
+        Arrays.sort(arr, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+        if (arr[0].equals("0")) return "0";
+        for (String s : arr) {
+            answer.append(s);    
         }
-        return sb.toString();
+        return answer.toString();
     }
 }
