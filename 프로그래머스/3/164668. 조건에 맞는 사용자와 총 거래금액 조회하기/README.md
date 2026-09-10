982faec9563a937c37d0c95140b0aa05c6f4a38d
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2026-09-11 01:04:52 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/README.md
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 조건에 맞는 사용자와 총 거래금액 조회하기 - 164668 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164668?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164668) 
 
 ### 성능 요약
 
-메모리: 0.0 MB, 시간: 0.00 ms
+메모리: undefined, 시간: 
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 18일 19:27:20
+2026년 09월 11일 01:04:48
 
 ### 문제 설명
 
--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,19 +1,8 @@
-SELECT
-    u.user_id as USER_ID,
-    u.nickname as NICKNAME,
-    sum(b.price) as TOTAL_SALES
-FROM
-    USED_GOODS_USER u
-JOIN
-    USED_GOODS_BOARD b
-    ON
-    u.user_id = b.writer_id
-WHERE
-    b.status = 'DONE'
-GROUP BY
-    u.user_id, u.nickname
-HAVING
-    sum(b.price) >= 700000
-ORDER BY
-    TOTAL_SALES
-;
+SELECT U.USER_ID, U.NICKNAME, SUM(B.PRICE) AS TOTAL_SALES
+FROM USED_GOODS_USER U
+JOIN USED_GOODS_BOARD B
+    ON U.USER_ID = B.WRITER_ID
+WHERE STATUS = 'DONE'
+GROUP BY U.USER_ID, U.NICKNAME
+HAVING SUM(B.PRICE) >= 700000
+ORDER BY TOTAL_SALES
