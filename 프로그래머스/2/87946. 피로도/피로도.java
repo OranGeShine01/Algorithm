178f2e476388f364728941e543834b6468a47def
Sejin Park <114332666+OranGeShine01@users.noreply.github.com>
2026-09-11 17:40:38 +0900

    [level 2] Title: 피로도, Time: 2.03 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    
+    int answer = 0;
+    int len = 0;
+    boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        len = dungeons.length;
+        visited = new boolean[len];
+        dfs(k, dungeons, 0);
+        return answer;
+    }
+    
+    void dfs(int k, int[][] dungeons, int depth) {
+        if (depth == len) {
+            answer = Math.max(answer, depth);
+            return;
+        }
+        for (int i = 0; i < len; i++) {
+            if (visited[i] || k < dungeons[i][0]) continue;
+            visited[i] = true;
+            dfs(k - dungeons[i][1], dungeons, depth + 1);
+            visited[i] = false;
+        }
+        answer = Math.max(answer, depth);
+    }
+}
